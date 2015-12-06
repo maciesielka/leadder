8abf5bf589ecd03903852e296c1e87ecfb744533
Michael Ciesielka <maciesielka@gmail.com>
2015-12-06 11:51:10 -0800

    Updated styling

--- src/controllers/profile-controller.js
+++ src/controllers/profile-controller.js
@@ -10,9 +10,20 @@ angular
         vm.matches = matches;
 
         vm.numWins = matches.reduce(function(prev, curr, index, arr) {
-            if ( curr.get('player1').get('user').get('fullName') && curr.get('player2').get('user').get('fullName')) {
-                return curr.get("player1").get("user").id === vm.user.id ? 1 : 0;
+            if ( !curr.get('player1') || !curr.get('player2')) return prev;
+            if ( curr.get('player1').get('user') && curr.get('player2').get('user')) {
+                var thisIteration = curr.get("player1").get("user").id === vm.user.id ? 1 : 0;
+                return prev + thisIteration;
             }
-            return 0;
-        });
+            return prev;
+        }, 0);
+
+        vm.numLosses = matches.reduce(function(prev, curr, index, arr) {
+            if ( !curr.get('player1') || !curr.get('player2')) return prev;
+            if ( curr.get('player1').get('user') && curr.get('player2').get('user')) {
+                var thisIteration = curr.get("player1").get("user").id === vm.user.id ? 0 : 1;
+                return prev + thisIteration;
+            }
+            return prev;
+        }, 0);
     });
